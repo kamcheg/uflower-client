5077f2df6ae064618265b6d7c84747ab909b675a
Kamil Akhmedkhanov <=>
2025-03-11 22:31:24 +0300

    fix eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,23 +25,21 @@ export default createConfigForNuxt({
       }],
 
       // порядок свойств
-      'vue/attributes-order': ['error', {
-        order: [
-          'DEFINITION',
-          'LIST_RENDERING',
-          'CONDITIONALS',
-          'RENDER_MODIFIERS',
-          'GLOBAL',
-          ['UNIQUE', 'SLOT'],
-          'TWO_WAY_BINDING',
-          'OTHER_DIRECTIVES',
-          'OTHER_ATTR',
-          'EVENTS',
-          'CONTENT',
-        ],
-        alphabetical: false,
-      }],
-
-      // кама
+      // 'vue/attributes-order': ['error', {
+      //   order: [
+      //     'DEFINITION',
+      //     'LIST_RENDERING',
+      //     'CONDITIONALS',
+      //     'RENDER_MODIFIERS',
+      //     'GLOBAL',
+      //     ['UNIQUE', 'SLOT'],
+      //     'TWO_WAY_BINDING',
+      //     'OTHER_DIRECTIVES',
+      //     'OTHER_ATTR',
+      //     'EVENTS',
+      //     'CONTENT',
+      //   ],
+      //   alphabetical: false,
+      // }],
     },
   })
