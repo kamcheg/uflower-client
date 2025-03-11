1f065c4d11798330cefc76ba44e36e0a72c03aa6
Kamil Akhmedkhanov <=>
2025-03-11 22:39:29 +0300

    fix eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,33 +13,5 @@ export default createConfigForNuxt({
     rules: {
       // Разрешается называть компонент одним словом
       'vue/multi-word-component-names': 'off',
-
-      // Если более двух html свойств, то на разных строках
-      'vue/max-attributes-per-line': ['error', {
-        singleline: {
-          max: 1,
-        },
-        multiline: {
-          max: 1,
-        },
-      }],
-
-      // порядок свойств
-      // 'vue/attributes-order': ['error', {
-      //   order: [
-      //     'DEFINITION',
-      //     'LIST_RENDERING',
-      //     'CONDITIONALS',
-      //     'RENDER_MODIFIERS',
-      //     'GLOBAL',
-      //     ['UNIQUE', 'SLOT'],
-      //     'TWO_WAY_BINDING',
-      //     'OTHER_DIRECTIVES',
-      //     'OTHER_ATTR',
-      //     'EVENTS',
-      //     'CONTENT',
-      //   ],
-      //   alphabetical: false,
-      // }],
     },
   })
