71217ac25f67d49a243b44a6de7d96566b4101a0
Kamil Akhmedkhanov <=>
2025-03-12 01:56:22 +0300

    fix localstorage (cookie)

--- src/entities/favorites/model/store.ts
+++ src/entities/favorites/model/store.ts
@@ -1,8 +1,12 @@
-import { useLocalStorage } from '@vueuse/core'
 import type { IProduct } from '~/entities/product'
 
 export const useFavoritesStore = defineStore('favoritesStore', () => {
-  const list = useLocalStorage<IProduct['id'][]>('favorites', [])
+  const list = useCookie<IProduct['id'][]>('favorites', {
+    default: () => [], // Значение по умолчанию
+    maxAge: 3600 * 24 * 3, // Время жизни cookie в секундах (1 час)
+    secure: true, // Только для HTTPS
+    sameSite: 'strict', // Политика безопасности
+  })
 
   return {
     list,
