1358b5903b126a770ed78bcbb4fff036586239c4
Kamil Akhmedkhanov <=>
2025-03-13 14:14:25 +0300

    init

--- src/entities/product/ui/card/ProductCard.vue
+++ src/entities/product/ui/card/ProductCard.vue
@@ -74,53 +74,57 @@ const dialogVisible = ref(false)
 
 <style scoped lang="scss">
 .product-card {
-    .image-place {
+  .image-place {
+    width: 100%;
+    padding-bottom: 100%;
+    position: relative;
+
+    &__inner {
+      position: absolute;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      overflow: hidden;
+      border-radius: 8px;
+      height: 100%;
+      width: 100%;
+
+      img {
+        display: block;
+        object-fit: cover;
+        height: 100%;
         width: 100%;
-        padding-bottom: 100%;
-        position: relative;
-
-        &__inner {
-            position: absolute;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            overflow: hidden;
-            border-radius: 8px;
-            height: 100%;
-            width: 100%;
-
-            img {
-                display: block;
-                object-fit: cover;
-                height: 100%;
-                width: 100%;
-            }
-        }
-
-        &__btns {
-            position: absolute;
-            top: 10px;
-            right: 10px;
-        }
+      }
     }
 
-    .title {
-        margin-top: 16px;
+    &__btns {
+      position: absolute;
+      top: 10px;
+      right: 10px;
     }
+  }
 
-    .price {
-        font-size: 18px;
-        font-weight: 600;
-        margin-top: 16px;
+  .title {
+    margin-top: 16px;
+    max-width: 100%;
+    white-space: nowrap; /* Запрещает перенос текста */
+    overflow: hidden; /* Скрывает лишний текст */
+    text-overflow: ellipsis; /* Добавляет троеточие */
+  }
+
+  .price {
+    font-size: 18px;
+    font-weight: 600;
+    margin-top: 16px;
+  }
+
+  .btns {
+    margin-top: 16px;
+    display: flex;
+
+    &__item {
+      flex-grow: 1;
     }
-
-    .btns {
-        margin-top: 16px;
-        display: flex;
-
-        &__item {
-            flex-grow: 1;
-        }
-    }
+  }
 }
 </style>
--- src/pages/index/ui/TemplateCatalog.vue
+++ src/pages/index/ui/TemplateCatalog.vue
@@ -35,7 +35,7 @@ defineProps<{
   .catalog {
     margin-top: 24px;
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(5, minmax(0, 1fr));
     grid-gap: 24px;
   }
 
