255bc50e80e2ae031c533f2000396d5ff5323337
Kamil Akhmedkhanov <=>
2025-03-13 14:06:00 +0300

    init

--- src/pages/index/ui/PageHome.vue
+++ src/pages/index/ui/PageHome.vue
@@ -1,9 +1,54 @@
 <script setup lang="ts">
+import TemplateCatalog from '~/pages/index/ui/TemplateCatalog.vue'
+import { ProductCard } from '~/entities/product'
+import { products } from '~/mock'
 </script>
 
 <template>
-  <h1>Home</h1>
+  <div class="page-home">
+    <TemplateCatalog title="Собрали сегодня">
+      <ProductCard
+        v-for="p of products.slice(2, 4)"
+        :key="p.id"
+        :data="p"
+      />
+    </TemplateCatalog>
+
+    <TemplateCatalog
+      title="Новинки"
+      style="margin-top: 48px;"
+    >
+      <ProductCard
+        v-for="p of products.slice(14, 19)"
+        :key="p.id"
+        :data="p"
+      />
+    </TemplateCatalog>
+
+    <TemplateCatalog
+      title="Букеты до 5000"
+      style="margin-top: 48px;"
+    >
+      <ProductCard
+        v-for="p of products.slice(14, 19)"
+        :key="p.id"
+        :data="p"
+      />
+    </TemplateCatalog>
+
+    <TemplateCatalog
+      title="Популярные"
+      style="margin-top: 48px;"
+    >
+      <ProductCard
+        v-for="p of products.slice(14, 19)"
+        :key="p.id"
+        :data="p"
+      />
+    </TemplateCatalog>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.page-home {}
 </style>
---
+++ src/pages/index/ui/TemplateCatalog.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { Right } from '@element-plus/icons-vue'
+
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="template">
+    <div class="title">
+      {{ title }}
+    </div>
+
+    <div class="catalog">
+      <slot />
+    </div>
+
+    <NuxtLink
+      to="/catalog"
+      class="template-link"
+    >
+      Перейти в каталог<ElIcon class="template-link__icon"><Right /></ElIcon>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.template {
+  .title {
+    font-size: 24px;
+    font-weight: 600;
+  }
+
+  .catalog {
+    margin-top: 24px;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 24px;
+  }
+
+  &-link {
+    cursor: pointer;
+    transition-duration: 0.3s;
+    display: inline-flex;
+    align-items: center;
+    margin-top: 18px;
+    padding: 10px 24px;
+    background: #fff;
+    border-radius: 100px;
+    box-shadow: rgba(53, 64, 71, 0.1) 0px 6px 20px 0px;
+
+    &:hover {
+      box-shadow: rgba(53, 64, 71, 0.2) 0px 10px 20px 0px;
+    }
+
+    &__icon {
+      margin-left: 12px;
+    }
+  }
+}
+</style>
