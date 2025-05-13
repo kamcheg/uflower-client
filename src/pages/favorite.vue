6e20b9ac3c1867f6094409b6329c8ec11f9221ac
Kamil Akhmedkhanov <=>
2025-05-13 22:41:49 +0300

    fix

--- src/pages/favorite.vue
+++ src/pages/favorite.vue
@@ -37,29 +37,64 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1>Избранное</h1>
+  <div v-if="products.length" class="catalog">
+    <h1>Избранное</h1>
+    <div class="catalog__container">
+      <ProductCard
+        v-for="i of products"
+        :key="i.id"
+        :data="i"
+      >
+        <template #favorite-button>
+          <AddToFavorites :id="i.id" />
+        </template>
+        <template #cart-button>
+          <AddToCart :id="i.id" />
+        </template>
+      </ProductCard>
+    </div>
+  </div>
 
-  <div class="catalog">
-    <ProductCard
-      v-for="i of products"
-      :key="i.id"
-      :data="i"
-    >
-      <template #favorite-button>
-        <AddToFavorites :id="i.id" />
-      </template>
-      <template #cart-button>
-        <AddToCart :id="i.id" />
-      </template>
-    </ProductCard>
+  <div
+    v-else
+    class="page-favorite-empty"
+  >
+    <div class="container">
+      <h1>
+        Список избранных букетов пока пуст.<br/>Отметьте понравившиеся букеты для удобства.
+      </h1>
+
+      <ElLink
+        href="/catalog"
+        type="primary"
+      >
+        Перейти в каталог
+      </ElLink>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .catalog {
-  margin-top: 24px;
-  display: grid;
-  grid-template-columns: repeat(5, minmax(0, 1fr));
-  grid-gap: 24px;
+  &__container {
+    margin-top: 24px;
+    display: grid;
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+    grid-gap: 24px;
+  }
+}
+
+.page-favorite-empty {
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 60px;
+
+    h1 {
+      text-align: center;
+      margin-bottom: 24px;
+    }
+  }
 }
 </style>
