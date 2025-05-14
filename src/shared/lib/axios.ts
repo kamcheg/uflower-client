6ede5e3601e60c0ff323914c506faa6ee017aa41
Kamil Akhmedkhanov <=>
2025-05-14 12:13:05 +0300

    fix

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -1,11 +1,8 @@
 import axios from 'axios'
-import qs from 'qs';
 
 export const apiInstance = axios.create({
   baseURL: 'http://localhost:4000',
   headers: {
     ['Brand-Slug']: 'zizi'
   },
-  paramsSerializer: params =>
-    qs.stringify(params, { arrayFormat: 'repeat' })
 })
