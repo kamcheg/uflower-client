95d375738e8192761b142d70f5284d5c329a2d7f
Kamil Akhmedkhanov <=>
2025-05-22 17:22:56 +0300

    fix

--- src/app.vue
+++ src/app.vue
@@ -1,31 +1,26 @@
 <script lang="ts" setup>
-// import {useSizesStore} from "~/shared/stores/useSizesStore";
-// import {useRecipientsStore} from "~/shared/stores/useRecipientsStore";
-// import {useReasonsStore} from "~/shared/stores/useReasonsStore";
-// import {useFlowerTypesStore} from "~/shared/stores/useFlowerTypesStore";
-//
-// const sizesStore = useSizesStore()
-// const recipientsStore = useRecipientsStore()
-// const reasonsStore = useReasonsStore()
-// const flowerTypesStore = useFlowerTypesStore()
-//
-// onServerPrefetch(async () => {
-//   await sizesStore.fetch()
-//   await recipientsStore.fetch()
-//   await reasonsStore.fetch()
-//   await flowerTypesStore.fetch()
-// })
+import {useSizesStore} from "~/shared/stores/useSizesStore";
+import {useRecipientsStore} from "~/shared/stores/useRecipientsStore";
+import {useReasonsStore} from "~/shared/stores/useReasonsStore";
+import {useFlowerTypesStore} from "~/shared/stores/useFlowerTypesStore";
 
-import {fetchAbout, type IResponse} from "~/shared/api/fetchAbout";
+const sizesStore = useSizesStore()
+const recipientsStore = useRecipientsStore()
+const reasonsStore = useReasonsStore()
+const flowerTypesStore = useFlowerTypesStore()
 
-const { data, error } = await useAsyncData<IResponse>(
-  'about',
-  () => fetchAbout(),
-)
+onServerPrefetch(async () => {
+  await sizesStore.fetch()
+  await recipientsStore.fetch()
+  await reasonsStore.fetch()
+  await flowerTypesStore.fetch()
+})
 </script>
 
 <template>
-  <div></div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
 <style lang="scss">
